a17d2fa90507058e967938260a0e623c1ca12c39
Leonel Binique <leonel.binique@gmail.com>
2022-01-19 11:35:52 +0100

    Mise à jour question Quizz2

--- lib/quizzBrain2.dart
+++ lib/quizzBrain2.dart
@@ -3,10 +3,10 @@ import 'question.dart';
 class  QuizzBrain2 {
   int _questionNumber2 = 0;
   List<Question>_Questions2= [
-    Question('Question 1', true),
-    Question('Bienvenue', false),
-    Question('Question 3', false),
-    Question('Question 4', true),
+    Question('La chanteuse Céline Dion est née dans la ville de Charlemagne au Québec ?', true),
+    Question("La dengue est une maladie transmise par la piqûre d'une tique ?", false),
+    Question("L'est de l'Espagne est bordé par l'océan Atlantique ?", false),
+    Question("Le premier être vivant à aller dans l'Espace est la chienne Laïka, envoyée par les Américains ?", false),
   ];
 
   String getQuestionText2() {
